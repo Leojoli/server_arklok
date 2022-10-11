596874877ec4ba19dccbeb50c88f2f21cf5738a6
Leonardo Lima <leonardo.guns@gmail.com>
2022-10-11 10:19:19 -0300

    dia e mes

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ async function criaServico() {
     var data = new Date();
     data.setDate(data.getDate());
     let diaSplit = data.toLocaleString().slice(0, 10).split("/")
-    let diaAtual = diaSplit[0] < 10 ? "0" + diaSplit[0] : diaSplit[0] + "/" + diaSplit[1] < 10 ? "0" + diaSplit[0] : diaSplit[1] + "/" + diaSplit[2]
+    let diaAtual = diaSplit[0] + "/" + diaSplit[1] + "/" + diaSplit[2]
     data.setDate(data.getDate() - 14);
     let diaAnteriorSplit = data.toLocaleString().slice(0, 10).split("/")
-    let diaAnterior = diaAnteriorSplit[0] < 10 ? "0" + diaAnteriorSplit[0] : diaAnteriorSplit[0] + "/" + diaAnteriorSplit[1] < 10 ? "0" + diaAnteriorSplit[0] : diaAnteriorSplit[1] + "/" + diaAnteriorSplit[2]
+    let diaAnterior = diaAnteriorSplit[1] + "/" + diaAnteriorSplit[0] + "/" + diaAnteriorSplit[2]
     console.log(diaAnterior.replace(",", ""), diaAtual.replace(", ", ""));
 
     const obterDados = require('./obterDadosCriar')
@@ -395,10 +395,10 @@ async function atualizaServico() {
     var data = new Date();
     data.setDate(data.getDate());
     let diaSplit = data.toLocaleString().slice(0, 10).split("/")
-    let diaAtual = diaSplit[0] < 10 ? "0" + diaSplit[0] : diaSplit[0] + "/" + diaSplit[1] < 10 ? "0" + diaSplit[0] : diaSplit[1] + "/" + diaSplit[2]
+    let diaAtual = diaSplit[0] + "/" + diaSplit[1] + "/" + diaSplit[2]
     data.setDate(data.getDate() - 14);
     let diaAnteriorSplit = data.toLocaleString().slice(0, 10).split("/")
-    let diaAnterior = diaAnteriorSplit[0] < 10 ? "0" + diaAnteriorSplit[0] : diaAnteriorSplit[0] + "/" + diaAnteriorSplit[1] < 10 ? "0" + diaAnteriorSplit[0] : diaAnteriorSplit[1] + "/" + diaAnteriorSplit[2]
+    let diaAnterior = diaAnteriorSplit[1] + "/" + diaAnteriorSplit[0] + "/" + diaAnteriorSplit[2]
     console.log(diaAnterior.replace(",", ""), diaAtual.replace(", ", ""));
 
     //obtendo dados
